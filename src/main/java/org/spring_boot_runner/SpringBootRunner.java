990938b0b4e4d1d07c44c07b9fff3440ab9baf7e
Administrator <Administrator@windows10.microdone.cn>
2017-06-20 17:24:12 +0800

    创建项目

---
+++ src/main/java/org/spring_boot_runner/SpringBootRunner.java
@@ -0,0 +1,27 @@
+package org.spring_boot_runner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.ApplicationContext;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+//@EnableJpaRepositories("repository")
+//@EntityScan("model")
+@RestController
+public class SpringBootRunner {	
+	static ApplicationContext applicationContext;
+    public static void main( String[] args ) {
+    	applicationContext = SpringApplication.run(SpringBootRunner.class, args);
+    	
+    	
+        System.out.println( "Hello World!" );
+    }
+}
