87e93ecfdf467fff21856a7a5addfd9abd132b04
excv8426 <598867208@qq.com>
2017-06-26 14:35:30 +0800

    读取大Excel

---
+++ src/main/java/excel/SheetHandlerFactory.java
@@ -0,0 +1,63 @@
+package excel;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.poi.ss.util.CellAddress;
+import org.apache.poi.ss.util.CellReference;
+import org.apache.poi.xssf.eventusermodel.XSSFSheetXMLHandler.SheetContentsHandler;
+import org.apache.poi.xssf.usermodel.XSSFComment;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SheetHandlerFactory {
+	private NamedParameterJdbcTemplate jdbcTemplate;
+	
+	public SheetContentsHandler createSheetContentsHandler(){
+		return new ImportSheetHandler();
+	}
+	
+	private class ImportSheetHandler implements SheetContentsHandler {
+	    private boolean firstCellOfRow = false;
+	    private int currentRow = -1;
+	    private int currentCol = -1;
+	    private Map<Integer, String> rowMap;
+
+		@Override
+		public void startRow(int rowNum) {
+			this.firstCellOfRow = true;
+			this.currentRow = rowNum;
+	        this.currentCol = -1;
+		}
+
+		@Override
+		public void endRow(int rowNum) {
+			System.out.println("endRow");
+			System.out.println(this.rowMap);
+		}
+
+		@Override
+		public void cell(String cellReference, String formattedValue, XSSFComment comment) {
+			if(cellReference == null) {
+	            cellReference = new CellAddress(this.currentRow, this.currentCol).formatAsString();
+	        }
+	        int thisCol = (new CellReference(cellReference)).getCol();
+	        this.currentCol = thisCol;
+			importTo(cellReference,formattedValue);
+		}
+
+		@Override
+		public void headerFooter(String text, boolean isHeader, String tagName) {
+
+		}
+		
+		private void importTo(String cellReference, String formattedValue){
+			if (this.firstCellOfRow) {
+				this.firstCellOfRow=false;
+				this.rowMap=new HashMap<>();
+			}
+			this.rowMap.put(this.currentCol, formattedValue);
+		}
+	}
+}
